a4c5ea62bc6f46aa299201ebc3acb9566911873c
Kamila Faria <Kamila Faria@franqueadora1.yes.com>
2025-06-11 16:53:23 -0300

    add assets do site da yes

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-yellow-100">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind funcionando!</h1>
+    </main>
+  );
+}
